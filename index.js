71ded04cb2ab1726d1c226e3eef95a9588b3b027
prodcontrol <37222945+prodcontrol@users.noreply.github.com>
2018-03-23 08:06:51 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const restService = express();
+
+restService.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
